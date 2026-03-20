cd2d39771b8f79a192bd0cdf5751adba7cb5a5f6
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2026-03-21 00:07:13 +0530

    Setup DB and created 'User' model

--- Server/user/dist/server.js
+++ Server/user/dist/server.js
@@ -1,6 +1,10 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
+connectDB();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
--- Server/user/src/server.ts
+++ Server/user/src/server.ts
@@ -1,9 +1,15 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-const app = express()
+dotenv.config();
 
-const PORT = 5000
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
