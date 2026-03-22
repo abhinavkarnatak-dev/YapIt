8031d0221de297d028130e8ce72b8a65ca70c431
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2026-03-22 13:31:36 +0530

    Added Redis via Upstash

--- Server/user/dist/server.js
+++ Server/user/dist/server.js
@@ -1,8 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { createClient } from "redis";
 dotenv.config();
 connectDB();
+export const redisClient = createClient({
+    url: process.env.REDIS_URL,
+});
+redisClient.connect().then(() => {
+    console.log("Connected to Redis");
+}).catch((err) => {
+    console.log("Redis Client Error", err);
+});
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
--- Server/user/src/server.ts
+++ Server/user/src/server.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { createClient } from "redis";
 
 dotenv.config();
 
 connectDB();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient.connect().then(() => {
+  console.log("Connected to Redis");
+}).catch((err) => {
+  console.log("Redis Client Error", err);
+});
 
 const app = express();
 
